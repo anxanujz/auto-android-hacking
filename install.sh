5c052c6877d8b126e424f80f6a7d81a6019e293f
anxanujz <112464718+anxanujz@users.noreply.github.com>
2022-08-31 06:15:57 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+# !bin/bash
+# Author : Anuj
+# This script installs all essential components for using Android Hacking Payload.
+
+pkg update -y
+pkg upgrade -y
+pkg install unstable-repo -y
+termux-setup-storage
+pkg install metasploit -y
+pkg install wget openssh apksigner nano -y
